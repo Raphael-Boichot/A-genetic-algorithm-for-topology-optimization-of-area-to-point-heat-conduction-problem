8befe51794b9fa06118081941d0afa931675f72b
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2024-01-23 17:08:40 +0100

    Add files via upload

--- Codes/Algo_genetique_random_start.m
+++ Codes/Algo_genetique_random_start.m
@@ -6,18 +6,18 @@ close all;
 k0=1;
 kp_k0=10;
 p=1e6;
-pas_x=0.001;
+step_x=0.001;
 T_ref=298;
-taux_remplissage=0.3;
+filling_ratio=0.3;
 imname='50x100.bmp';
 %--------------------------------------------------------------------------
 
 %--------Hyper parameters for genetic algorithm----------------------------
-taille_pop=1000;
-meilleurs=200;
+population_size=1000;
+population_best=200;
 nb_generations=10000;
 prob_croisement=0.2;
-probabilite_mutation_maximale=0.1;
+population_bestimale=0.1;
 %--------------------------------------------------------------------------
 
 rng('shuffle', 'twister')
@@ -27,29 +27,29 @@ mkdir('Average_topology');
 
 
 T_comp=0;
-table=zeros(taille_pop,2);
-couleurs=imread(imname);
-[hauteur,largeur,profondeur]=size(couleurs);
-conditi_limites_ini=zeros(hauteur,largeur);
-pixels_blancs=0;
-for k = 1:1:hauteur
-    for l = 1:1:largeur
+table=zeros(population_size,2);
+pixels=imread(imname);
+[height,width,layers]=size(pixels);
+Initial_boundary_limits=zeros(height,width);
+non_conductive_pixels=0;
+for k = 1:1:height
+    for l = 1:1:width
         
-        rouge = couleurs(k,l,1);
-        vert = couleurs(k,l,2);
-        bleu = couleurs(k,l,3);
+        red = pixels(k,l,1);
+        green = pixels(k,l,2);
+        blue = pixels(k,l,3);
         
-        if (rouge == 127) && (vert == 127) && (bleu == 127)
-            conditi_limites_ini(k,l)=-2;
+        if (red == 127) && (green == 127) && (blue == 127)
+            Initial_boundary_limits(k,l)=-2;
         end
         
-        if (rouge == 0) && (vert == 0) && (bleu == 255)
-            conditi_limites_ini(k,l)=-3;
+        if (red == 0) && (green == 0) && (blue == 255)
+            Initial_boundary_limits(k,l)=-3;
         end
         
-        if (rouge == 255) && (vert == 255) && (bleu == 255)
-            conditi_limites_ini(k,l)=k0;
-            pixels_blancs=pixels_blancs+1;
+        if (red == 255) && (green == 255) && (blue == 255)
+            Initial_boundary_limits(k,l)=k0;
+            non_conductive_pixels=non_conductive_pixels+1;
         end
         
     end
@@ -59,14 +59,14 @@ end
 %initial population creation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-population=zeros(hauteur, largeur,taille_pop);
-nombre_pixels_conducteurs=ceil(pixels_blancs*taux_remplissage);
+population=zeros(height, width,population_size);
+conductive_pixels=ceil(non_conductive_pixels*filling_ratio);
 disp('Creating the initial population frow scratch...');
 tic
-parfor i=1:1:taille_pop
-    population(:,:,i)=init_image(conditi_limites_ini,nombre_pixels_conducteurs, k0, kp_k0);
+parfor i=1:1:population_size
+    population(:,:,i)=init_image(Initial_boundary_limits,conductive_pixels, k0, kp_k0);
 end
-topology_history=zeros(hauteur, largeur, nb_generations);
+topology_history=zeros(height, width, nb_generations);
 toc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Fitness evaluation and sorting by fitness
@@ -78,92 +78,94 @@ for g=1:1:nb_generations
     
     %Mutation rate is decreased with epoch following an empirical law that
     %works well on this problem
-    prob_mutation=probabilite_mutation_maximale*exp(-5.8*g/nb_generations);
+    prob_mutation=population_bestimale*exp(-5.8*g/nb_generations);
     
     %Fitness calculation
     disp('Calculating fitness for each individual...');
     % Variables in this order : variance, moyenne, temperature_max, map
     % temperatures, map gradients, variance gradients
-    parfor i=1:1:taille_pop
-        [~,~, ~, variance_border,~, ~,t_max, temp, ~,var_grad]=finite_temp_direct_sparse(k0*kp_k0,k0,T_ref,pas_x,p,population(:,:,i));
+    parfor i=1:1:population_size
+        [~,~, ~, variance_border,~, ~,t_max, temp, ~,var_grad]=finite_temp_direct_sparse(k0*kp_k0,k0,T_ref,step_x,p,population(:,:,i));
         fitness(i,g)=t_max;
     end
     
     %Best topologies are kept for next step
-    temp_temp=[(1:1:taille_pop)',fitness(:,g)];
+    temp_temp=[(1:1:population_size)',fitness(:,g)];
     pop_classe=sortrows(temp_temp, 2);
-    indice=pop_classe(1:meilleurs,1);
+    indice=pop_classe(1:population_best,1);
     opti_p_crois=table(indice(1),1);
     opti_p_mut=table(indice(1),2);
+
+    %elitism
+    new_population(:,:,1)=population(:,:,indice(1));
+    new_population(:,:,2)=population(:,:,indice(1));
     
     if (g>1)
         if (fitness(1,g)==fitness(1,g-1))
-            disp('No best configuration detected')
+             disp('---------No better configuration detected---------')
+            %disp('Applying the ESO algorithm')
+            %[ESO_shape,growth,etching] = fun_ESO_algorithm(population(:,:,indice(1)),k0*kp_k0,k0,T_ref,step_x,p);
+            %new_population(:,:,1)=ESO_shape;
         else
-            disp('Best configuration detected')
+            disp('------------Better configuration detected----------')
         end
     end
     
-    %elitism with two times the best topology, you'll see why after
-    nouvelle_population(:,:,1)=population(:,:,indice(1));
-    nouvelle_population(:,:,2)=population(:,:,indice(1));
-    topology_history(:,:,g)=population(:,:,indice(1));
-    %For keeping trace of the hyper parameters leading to success
-    
+    topology_history(:,:,g)=new_population(:,:,1);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %New population of childrens
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     disp('Applying the mutation/crossover algorithm...');
-    parfor m=3:1:(taille_pop)
-        [nouvelle_population(:,:,m),table(m,:)]=gene_enfant(population,kp_k0,k0, nombre_pixels_conducteurs, prob_mutation, prob_croisement, meilleurs,indice);
+    parfor m=3:1:(population_size)
+        [new_population(:,:,m),table(m,:)]=gene_enfant(population,kp_k0,k0, conductive_pixels, prob_mutation, prob_croisement, population_best,indice);
     end
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %get the best topology
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    meilleure_image=nouvelle_population(:,:,1);
-    best_image=zeros(hauteur,largeur,3);
-    image_moyenne=zeros(hauteur,largeur,3);
+    best_topology=new_population(:,:,1);
+    best_image=zeros(height,width,3);
+    mean_topology=zeros(height,width,3);
     
-    somme_controle=0;
-    for k = 1:1:hauteur
-        for l = 1:1:largeur
+    checksum=0;
+    for k = 1:1:height
+        for l = 1:1:width
             
-            if meilleure_image(k,l)==k0
+            if best_topology(k,l)==k0
                 best_image(k,l,1)=255;
                 best_image(k,l,2)=255;
                 best_image(k,l,3)=255;
-                image_moyenne(k,l,1)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
-                image_moyenne(k,l,2)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
-                image_moyenne(k,l,3)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                mean_topology(k,l,1)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                mean_topology(k,l,2)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                mean_topology(k,l,3)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
             end
             
-            if meilleure_image(k,l)==k0*kp_k0
+            if best_topology(k,l)==k0*kp_k0
                 best_image(k,l,1)=0;
                 best_image(k,l,2)=0;
                 best_image(k,l,3)=0;
-                image_moyenne(k,l,1)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
-                image_moyenne(k,l,2)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
-                image_moyenne(k,l,3)=255-((mean(nouvelle_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
-                somme_controle=somme_controle+1;
+                mean_topology(k,l,1)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                mean_topology(k,l,2)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                mean_topology(k,l,3)=255-((mean(new_population(k,l,:))-k0)/(k0*kp_k0-k0))*255;
+                checksum=checksum+1;
             end
             
-            if meilleure_image(k,l)==-2
+            if best_topology(k,l)==-2
                 best_image(k,l,1)=127;
                 best_image(k,l,2)=127;
                 best_image(k,l,3)=127;
-                image_moyenne(k,l,1)=127;
-                image_moyenne(k,l,2)=127;
-                image_moyenne(k,l,3)=127;
+                mean_topology(k,l,1)=127;
+                mean_topology(k,l,2)=127;
+                mean_topology(k,l,3)=127;
             end
             
-            if meilleure_image(k,l)==-3
+            if best_topology(k,l)==-3
                 best_image(k,l,1)=0;
                 best_image(k,l,2)=0;
                 best_image(k,l,3)=255;
-                image_moyenne(k,l,1)=0;
-                image_moyenne(k,l,2)=0;
-                image_moyenne(k,l,3)=255;
+                mean_topology(k,l,1)=0;
+                mean_topology(k,l,2)=0;
+                mean_topology(k,l,3)=255;
                 
             end
             
@@ -171,11 +173,11 @@ for g=1:1:nb_generations
     end
     
     best_image=uint8(best_image);
-    image_moyenne=uint8(image_moyenne);
+    mean_topology=uint8(mean_topology);
     
-    miroir_best=fliplr(best_image(1:hauteur,1:largeur-1,:));
+    miroir_best=fliplr(best_image(1:height,1:width-1,:));
     miroir_best2=fliplr(miroir_best);
-    miroir_mean=fliplr(image_moyenne(1:hauteur,1:largeur-1,:));
+    miroir_mean=fliplr(mean_topology(1:height,1:width-1,:));
     miroir_mean2=fliplr(miroir_mean);
     
     if g==1
@@ -197,7 +199,7 @@ for g=1:1:nb_generations
     
     %clc;
     disp(['---------Epoch: ',num2str(g),'---------']);
-    disp(['Sum of conductive cells: ', num2str(nombre_pixels_conducteurs), ' must be equal to : ', num2str(somme_controle)]);
+    disp(['Sum of conductive cells: ', num2str(conductive_pixels), ' must be equal to : ', num2str(checksum)]);
     disp(['Current maximal mutation probability: ', num2str(prob_mutation)]);
     disp(['Last successfull - crossover rate: ', num2str(opti_p_crois), ' / mutation rate : ', num2str(opti_p_mut)]);
     disp(['Best fitness: ', num2str(fitness(1,g))]);
@@ -214,9 +216,9 @@ for g=1:1:nb_generations
     end
     
     if g>1
-        residus(1,g-1) = (fitness(1,g-1)-fitness(1,g))/norme_iteration_1;
+        residuals(1,g-1) = (fitness(1,g-1)-fitness(1,g))/norme_iteration_1;
         subplot(2,4,1);
-        plot(log10(residus), '.r');
+        plot(log10(residuals), '.r');
         title('Residuals');
         xlabel('Generation');
         ylabel('log10 value');
@@ -227,11 +229,11 @@ for g=1:1:nb_generations
     title('Best topology');
     
     subplot(2,4,3);
-    imagesc(image_moyenne);
+    imagesc(mean_topology);
     title('Average topology');
     
     subplot(2,4,4);
-    [distance,somme_entropie, entropie, border_variance, variance, moyenne,t_max,temp_affichage, grad, variance_grad]=finite_temp_direct_sparse(k0*kp_k0,k0,T_ref,pas_x,p,population(:,:,1));
+    [distance,somme_entropie, entropie, border_variance, variance, moyenne,t_max,temp_affichage, grad, variance_grad]=finite_temp_direct_sparse(k0*kp_k0,k0,T_ref,step_x,p,population(:,:,1));
     imagesc(temp_affichage);
     title('Objective function');
     
@@ -263,7 +265,7 @@ for g=1:1:nb_generations
     colormap jet
     
     pause(0.01);
-    population=nouvelle_population;
+    population=new_population;
     
     saveas(gcf,['Figure\Figure_',num2str(g,'%06.f')],'png');
     saveas(gcf,'Figure.png');
